84f9b1ed8c77cba07d067097f124fbb518b83ff3
Ƭнσмαѕ ѕнєƖву <60951494+Zeroday10@users.noreply.github.com>
2020-02-12 05:49:59 +0330

    Update process.php

--- process.php
+++ process.php
@@ -1,14 +1,10 @@
 <?php
 $email = $_POST['email'];
-//echo "$email";
-
 mkdir("uploader/".$email);
 $picname = $_FILES['pic']['name'];
 $array = explode('.',$picname);
-//var_dump($array);
 $ext = end($array);
 $newname = rand().".".$ext;
-//var_dump($newname);
 $from = $_FILES['pic']['tmp_name'];
 $to = "uploader/".$email."/".$newname;
 move_uploaded_file($from,$to);
